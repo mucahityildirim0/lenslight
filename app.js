6950e532ed830bce96899e70140398489fd70451
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-09-05 22:51:02 +0300

    lesson 01

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("index sayfası 2");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
